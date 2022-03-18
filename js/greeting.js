b9877ad27484f6b981471f6e2b0d34233b08b354
dolhim <eadorim@naver.com>
2022-03-19 00:27:30 +0900

    feat: greeting 추가

---
+++ js/greeting.js
@@ -0,0 +1,29 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onSubmit);
+} else {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(savedUserName);
+}
+
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerHTML = `Hello ${username}`;
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(userName);
+}
